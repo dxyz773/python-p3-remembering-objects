a4b5eb522f9d196ab0b9079957d80d7d8856ddff
demitryedwards <dxyz773@gmail.com>
2023-05-29 23:13:55 -0400

    complete code along

---
+++ app.py
@@ -0,0 +1,22 @@
+class Song:
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Song.add_song_to_all(self)
+
+    @classmethod
+    def add_song_to_all(cls, song):
+        cls.all.append(song)
+
+    @classmethod
+    def show_song_names(cls):
+        print([song.name for song in cls.all])
+
+
+big_energy = Song("Big Energy")
+bad_blood = Song("Bad Blood")
+ninety_nine_problems = Song("99 Problems")
+thriller = Song("Thriller")
+
+Song.show_song_names()
